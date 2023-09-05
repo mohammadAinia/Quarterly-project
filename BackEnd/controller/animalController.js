710f5c2d3bfc7bc76236d848be8a9dd2c5a72cff
Dragonmo <moohamad20-01@hotmail.com>
2023-09-05 22:17:40 +0300

    1

--- BackEnd/controller/animalController.js
+++ BackEnd/controller/animalController.js
@@ -28,7 +28,7 @@ function add_animal(req,res) {
         place: req.body.place,
         owner: req.body.owner,
         ownered: req.body.ownered,
-        userId: 1
+
     }//object for save in my db
     const schema={
         name: { type: "string",optional: false },
@@ -78,9 +78,39 @@ function destroy(req,res) {
         })
     });
 }
+function update(req,res) {
+    const id=req.params.id
+    const updateAnimal=
+    {
+        name: req.body.name,
+        type: req.body.type,
+        color: req.body.color,
+        age: req.body.age,
+        gender: req.body.gender,
+        food_type: req.body.food_type,
+        place: req.body.place,
+        owner: req.body.owner,
+        ownered: req.body.ownered,
+    }
+
+    models.animal.update(updateAnimal,{ where: { id: id } }).then(result => {
+        res.status(200).json(result
+            // {
+            //     message: "post suc",
+            //     // post:result
+            // }
+        )
+    }).catch(error => {
+        res.status(500).json({
+            message: "update fun err",
+            error: error
+        })
+    })
+}
 module.exports={
     add_animal: add_animal,
     destroy: destroy,
-    show_all: show_all
+    show_all: show_all,
+    update:update
 
 }
