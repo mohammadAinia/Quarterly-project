44a63d9b0c9f2372853d7bf024185b92074fa73d
Dragonmo <moohamad20-01@hotmail.com>
2023-09-08 14:26:07 +0300

    fg

--- BackEnd/models/user_info.js
+++ BackEnd/models/user_info.js
@@ -14,11 +14,13 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   user_info.init({
-    fullname: DataTypes.STRING,
+    // fullname: DataTypes.STRING,
+    first_name:DataTypes.STRING,
+    last_name : DataTypes.STRING ,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     phone: DataTypes.INTEGER,
-    rolee: DataTypes.STRING
+    // rolee: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'user_info',
