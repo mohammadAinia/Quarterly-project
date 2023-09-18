0f0783a1cb9d355f72c7dc0e997fbd3707b55f90
Dragonmo <moohamad20-01@hotmail.com>
2023-09-18 23:00:41 +0300

    lkkjlj

--- BackEnd/controller/common_problim.js
+++ BackEnd/controller/common_problim.js
@@ -1,14 +1,15 @@
 const models=require("../models")
 
-const show_problim=(req,res) => {
+const show_all_problim=(req,res) => {
     models.common_problim.findAll()
         .then(
             result => {
-                return res.json({ valid: true,ses: req.session.username })
+                return res.json({ valid: true,ses: req.session.username,result: result })
             }
         )
         .catch(
             err => {
+                console.log(err)
                 return res.json({ valid: false })
             }
         )
@@ -18,17 +19,47 @@ const add_problim=(ress,res) => {
         title: req.body.title,
         disc: req.body.desc,
         publisher: req.session.username,
-        animal_type: req.body.animal_type
+        animal_type: req.body.animal_type,
+        category: req.body.cat
     }
     models.common_problim.create(problim).then(
         res => {
-            return res.json({valid:true,ses:req.session.username})
+            return res.json({ valid: true,ses: req.session.username })
         }
     )
         .catch(err => {
+            console.log(err)
+
             return res.json({ valid: false })
         })
 }
-const answer=(req,res)=>{
-    
+const add_answer=(req,res) => {
+    const id_problim=req.body.id_problim
+    models.answer_table.findOne({ where: { id_problim: id_problim } }).then(
+        result => {
+            const ansewr={
+                ansewr: req.body.ansewr,
+                id_problim: result.id_problim
+            }
+            models.answer_table.create(ansewr).then(
+                result=>{
+                    return res.json({valid:true,result:result})
+                }
+            )
+                .cach(
+                    err => {
+                        console.log(err)
+                        return res.json({ valid: false })
+
+                    }
+                )
+        }
+    )
+        .cach(
+            err => {
+                console.log(err)
+
+                return res.json({ valid: false })
+            }
+        )
 }
--- BackEnd/controller/userController.js
+++ BackEnd/controller/userController.js
@@ -1,18 +1,15 @@
-const models=require("../models")
-const bcryptjs=require("bcryptjs")
+const models=require("../models");
+const bcryptjs=require("bcryptjs");
 const singup_vet=(req,res) => {
-    models.user_info.findOne({ where: { email: req.body.Email } }).then(
-        result => {
+    models.user_info.findOne({ where: { email: req.body.Email } }).then((result) => {
             if (result) {
                 res.json({
                     message: "email alrady exist ",
-                })
-            }
-            else {
+                });
+            } else {
                 bcryptjs.genSalt(10,(err,salt) => {
                     bcryptjs.hash(req.body.Password,salt,function (err,hash) {
                         const user={
-                            
                             first_name: req.body.First_name,
                             last_name: req.body.Last_name,
                             email: req.body.Email,
@@ -21,10 +18,9 @@ const singup_vet=(req,res) => {
                             rolee: "doc",
                             age: req.body.Age,
                             gender: req.body.Gender,
-                        }
+                        };
 
-                        models.user_info.create(user).then(
-                            result => {
+                        models.user_info.create(user).then((result) => {
                                 // res.status(200).json(result)
 
                                 const veterinarians={
@@ -35,57 +31,46 @@ const singup_vet=(req,res) => {
                                     exp: req.body.exp,
                                     num_year_exp: req.body.num_year_exp,
                                     deatalis: req.body.Previous_work,
-                                    url_bsc:req.file.bsc,
-                                    url_prev_imag:req.file.prev
-                                    
-                                }
-                                models.veterinariann.create(veterinarians).then(
-                                    result => {
-                                        req.session.username=user.email
-                                        return res.json(
-                                            { Login: true,username: req.session.username }
-                                        )
-                                    }
-                                )
-                                    .catch(error => {
+                                    url_bsc: req.file.bsc,
+                                    url_prev_imag: req.file.prev,
+                                };
+                                models.veterinariann.create(veterinarians).then((result) => {
+                                        req.session.username=user.email;
                                         return res.json({
-                                            valid: false
-                                        })
+                                            Login: true,
+                                            username: req.session.username,
+                                        });
                                     })
-
-
-                            }
-                        ).catch(error => {
-                            res.status(500).json({
-                                message: "somthing wrong1111"+error
-                                ,
-
+                                    .catch((error) => {
+                                        return res.json({
+                                            valid: false,
+                                        });
+                                    });
                             })
-                        })
-
-                    })
-                })
+                            .catch((error) => {
+                                res.status(500).json({
+                                    message: "somthing wrong1111"+error,
+                                });
+                            });
+                    });
+                });
             }
-        }
-    ).catch(
-        error => {
+        })
+        .catch((error) => {
             res.status(500).json({
                 message: "somthing wrong 500"+error,
-
-            })
-        }
-    )
-
-}
+            });
+        });
+};
 const singup_user=(req,res) => {
-    models.user_info.findOne({ where: { email: req.body.Email } }).then(
-        result => {
+    models.user_info
+        .findOne({ where: { email: req.body.Email } })
+        .then((result) => {
             if (result) {
                 return res.json({
                     message: "email user alrady exist ",
-                })
-            }
-            else {
+                });
+            } else {
                 bcryptjs.genSalt(10,(err,salt) => {
                     bcryptjs.hash(req.body.Password,salt,function (err,hash) {
                         const user={
@@ -97,154 +82,127 @@ const singup_user=(req,res) => {
                             rolee: "user",
                             age: req.body.Age,
                             gender: req.body.Gender,
-                        }
-                        models.user_info.create(user).then(
-                            result => {
-                                req.session.username=user.email
+                        };
+                        models.user_info
+                            .create(user)
+                            .then((result) => {
+                                req.session.username=user.email;
                                 // console.log(req.session.username)
-                                return res.json(
-
-                                    { Login: true,username: req.session.username }
-                                )
-                            }
-                        ).catch(error => {
-                            return res.json({
-                                valid: false
+                                return res.json({
+                                    Login: true,
+                                    username: req.session.username,
+                                });
                             })
-                        })
-                    })
-                })
+                            .catch((error) => {
+                                return res.json({
+                                    valid: false,
+                                });
+                            });
+                    });
+                });
             }
-        }
-    ).catch(
-        error => {
+        })
+        .catch((error) => {
             res.status(500).json({
                 message: "somthing wrong",
-
-            })
-        }
-    )
-
-}
+            });
+        });
+};
 function login(req,res) {
-
-    models.user_info.findOne({ where: { email: req.body.email } }).then(
-        user => {
+    models.user_info
+        .findOne({ where: { email: req.body.email } })
+        .then((user) => {
             if (user==null) {
                 return res.json({
                     message: "this email is not exist",
-                })
-            }
-            else if
-                (user.rolee=="DOC") {
-                bcryptjs.compare(req.body.password,user.password,function (err,result) {
-                    if (result) {
-                        req.session.username=user.email
-                        console.log(user.first_name)
-                        // console.log(req.session.username)
-                        return res.json({ Login: true,username: req.session.username });
-
+                });
+            } else if (user.rolee=="DOC") {
+                bcryptjs.compare(
+                    req.body.password,
+                    user.password,
+                    function (err,result) {
+                        if (result) {
+                            req.session.username=user.email;
+                            console.log(user.first_name);
+                            // console.log(req.session.username)
+                            return res.json({ Login: true,username: req.session.username });
+                        } else {
+                            return res.json({
+                                message: "incorrect password",
+                            });
+                        }
                     }
-                    else {
-                        return res.json({
-                            message: "incorrect password",
-
-                        })
+                );
+            } else if ((user.rolee="user")) {
+                bcryptjs.compare(
+                    req.body.password,
+                    user.password,
+                    function (err,result) {
+                        if (result) {
+                            req.session.username=user.email;
+                            console.log(user.first_name);
+                            return res.json({ Login: true,username: req.session.username });
+                        } else {
+                            return res.status(401).json({
+                                message: "incorrect password",
+                            });
+                        }
                     }
-                })
-            }
-            else if (user.rolee="user") {
-                bcryptjs.compare(req.body.password,user.password,function (err,result) {
-                    if (result) {
-                        req.session.username=user.email
-                        console.log(user.first_name)
-                        return res.json({ Login: true,username: req.session.username });
-                    }
-                    else {
-                        return res.status(401).json({
-                            message: "incorrect password",
-
-                        })
-                    }
-                })
-            }
-            else
-                return res.json({ Login: false })
-        }
-    ).catch(
-        error => {
+                );
+            } else return res.json({ Login: false });
+        })
+        .catch((error) => {
             return res.json({
                 message: "somthing wrong"+error,
-
-            })
-        }
-    )
+            });
+        });
 }
 
 function logout(req,res) {
-
-    res.status(200).json({ message: "ok is done" })
+    res.status(200).json({ message: "ok is done" });
     //redirect to the home page here
 }
 
 function show_users(req,res) {
-    models.user_info.findAll().then(
-        data => {
-            res.status(200).json({ data })
-        }
-    )
-        .catch(
-            err =>
-                res.status(404).json({ err })
-
-        )
+    models.user_info
+        .findAll()
+        .then((data) => {
+            res.status(200).json({ data });
+        })
+        .catch((err) => res.status(404).json({ err }));
 }
 
 const home_owner=(req,res) => {
     if (req.session.username) {
-        
-        models.user_info.findOne({where:{email:req.session.username}}).then(
-            resp=>{
-                models.animal.findAll({where:{owner:req.session.username}}).then(
-                    ress=>{
-                        models.common_problim.findAll().then(
-                            result=>{
-                        return res.json({ valid: true,username:resp.first_name ,name:ress.name,image:ress.urlImage,pro:result.title})
-                            }
-                        ).catch(
-                            err=>{
-                                return res.json({valid :false})
-                            }
-                        )
-                    }
-                
-                )
-                .catch(
-                    err=>{
-                        return  res.json({valid :false})
-                    }
-                )
-            }
-        ).catch(
-            err=>{
-                return  res.json({valid :false})
-            }
-        )
+        models.user_info.findOne({ where: { email: req.session.username } }).then((resp) => {
+                models.animal.findAll({ where: { owner: req.session.username } }).then((ress) => {
+                        models.common_problim.find()
+                            .then((result) => {
+                                return res.json({
+                                    valid: true,username: resp.first_name,name: ress.name,image: ress.urlImage,pro: result.title,
+                                });
+                            })
+                            .catch((err) => {
+                                return res.json({ valid: false });
+                            });
+                    })
+                    .catch((err) => {
+                        return res.json({ valid: false });
+                    });
+            })
+            .catch((err) => {
+                return res.json({ valid: false });
+            });
+    } else {
+        return res.json({ valid: false });
     }
-    else {
-        return res.json({ valid: false })
-    }
-}
+};
 
-
-
-
-module.exports=
-{
+module.exports={
     singup_user: singup_user,
     login: login,
     show_users: show_users,
     logout: logout,
     singup_vet: singup_vet,
-    home_owner: home_owner
-}
+    home_owner: home_owner,
+};
