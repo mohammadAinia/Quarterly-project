daa02c623a56f2af2e2060d2f44cb7728fc4eb4a
Dragonmo <moohamad20-01@hotmail.com>
2023-09-19 12:27:29 +0300

    شسييشس

--- front/src/Pages/Home_owner/Home_owner.js
+++ front/src/Pages/Home_owner/Home_owner.js
@@ -5,7 +5,6 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import animal_image from '../../Assert/Images/cute.avif'
 //ghghghghghghgh
-//عينية الكلبخهصيتبختيسبتنسيبمنسينمبنمتيس
 
 const Home_owner = () => {
   const [name, setName] = useState('')
