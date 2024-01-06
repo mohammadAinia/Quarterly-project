2e9557f019b9d1c5dd821811dbdec1672680715a
Dragonmo <moohamad20-01@hotmail.com>
2024-01-06 20:05:59 +0300

    dvvdvddv

--- BackEnd/controller/adoptionController.js
+++ BackEnd/controller/adoptionController.js
@@ -3,29 +3,39 @@ const db=require("../dbb/db")
 const add_to_adlist_id=(req,res)=>{
     const animal_id =req.params.id
     const user= req.session.username
+    var check='select * from adoprion where animal_id =?'
     var sql1='select * from animals where id=?'
-    db.query(sql1,[animal_id],(err,result2)=>{
-        if(err)console.log(err)
-        console.log(result2[0].owner)
-        var d=new Date()
-        var datee = d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate();
-        var sql= "insert into adoption (id_animal,current_owner,requester,status_id,shared_at) values ('" + animal_id + "','" + result2[0].owner + "','" + "" + "','" + 0 + "','" + datee + "')"
-        db.query(sql,(err,result)=>{
-            if(err){console.log(err+"err here is adoption")}
-            res.json(result)
+db.query(check,[animal_id],(error,resss)=>{
+    if (resss.length==0){
+        db.query(sql1,[animal_id],(err,result2)=>{
+            if(err)console.log(err)
+            console.log(result2[0].owner)
+            var d=new Date()
+            var datee = d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate();
+            var sql= "insert into adoption (id_animal,current_owner,requester,status_id,shared_at) values ('" + animal_id + "','" + result2[0].owner + "','" + "" + "','" + 0 + "','" + datee + "')"
+            db.query(sql,(err,result)=>{
+                if(err){console.log(err+"err here is adoption")}
+                res.json(result)
+            })
+    
         })
+    }
+    else {
+        res.json(chack)
 
-    })
+    }
+})
 
 }
 const show_ad_animal=(req,res)=>{
     var a=0
-    sql1="select animals.urlImage,animals.type,animals.age,animals.name,user_infos.phone,animals.id from adoption join animals on animals.id=adoption.id_animal JOIN user_infos on user_infos.email=animals.owner where adoption.status_id=? AND current_owner!=? "
+    sql1="select animals.urlImage,animals.type,animals.age,animals.name,user_infos.phone,animals.id from adoption join animals on animals.id=adoption.id_animal JOIN user_infos on user_infos.email=animals.owner where adoption.status_id=? AND current_owner!=? AND animals.id not in (SELECT animal_id from notifications where special=?)  "
 
-    db.query(sql1,[a,req.session.username],(err,result)=>{
-        if(err)console.log(err)
-        res.json({result,valid:true})
-    })
+                db.query(sql1,[a,req.session.username,req.session.username],(err,result)=>{
+                    res.json({result,valid:true})
+                })
+                    
+    
 }
 const adopt_an=(req,res)=>{
     var id=req.params.id
@@ -33,6 +43,7 @@ const adopt_an=(req,res)=>{
     db.query(qw,[id],(err,result)=>{
         var nr=req.session.username+'ask you to adopt your animal'+result[0].name
         var av= "insert into notifications (title,details,email,animal_id,special) values ('" + "adoption" + "','" + nr + "','" + result[0].owner + "','" + result[0].id + "','" + req.session.username + "')"
+
         db.query(av,(err,result)=>{
             res.json({result:"ok we will send the request for owner"})
         })
