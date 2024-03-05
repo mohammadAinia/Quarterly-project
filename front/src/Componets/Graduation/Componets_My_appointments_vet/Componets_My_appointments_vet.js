a3718857466ecadac833bf37a0d7d796a7495602
mohammadAinia <mmyyttt@gmail.com>
2024-03-06 00:46:31 +0300

    nn

--- front/src/Componets/Graduation/Componets_My_appointments_vet/Componets_My_appointments_vet.js
+++ front/src/Componets/Graduation/Componets_My_appointments_vet/Componets_My_appointments_vet.js
@@ -1,26 +1,7 @@
 import './Componets_My_appointments_vet.css'
 import My_AppointmentVector_36 from '../../../Assert/Images/My_AppointmentVector_36.png'
 import React from 'react'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
-import { useEffect, useState, useRef } from 'react'
 const Componets_My_appointments_vet = (props) => {
-
-    const [isDiagnosed, setIsDiagnosed] = useState(false);
-    const navigate = useNavigate()
-
-
-    // useEffect(() => {
-    //     axios.get(`http://localhost:3001/#/${props.id}`, { withCredentials: true })
-    //         .then(res => {
-    //             setIsDiagnosed(res.data.diagnosed);
-    //         })
-    //         .catch(err => {
-    //             console.log(err);
-    //             navigate('/login'); // Redirect to login page if there's an error
-    //         });
-    // }, [props.href, navigate]);
-
 
     return (
         <>
@@ -41,13 +22,13 @@ const Componets_My_appointments_vet = (props) => {
                 {/* <div class="text-wrapper-12">Time :</div> */}
                 <a className="nice-button" href={props.href}>Diagnosis</a>
 
-                {isDiagnosed && (
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="green-check">
-                        <path fill="#4CAF50" d="M9 16.17l-3.5-3.5 1.17-1.17 2.33 2.33 4.16-4.16 1.17 1.17z"/>
-                        <path d="M0 0h24v24H0z" fill="none"/>
-                        <animate attributeName="opacity" from="0.2" to="1" dur="1s" repeatCount="indefinite"/>
+                {props.test ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" viewBox="0 0 24 24" className="green-check">
+                        <path fill="#4CAF50" d="M9 16.17l-3.5-3.5 1.17-1.17 2.33 2.33 4.16-4.16 1.17 1.17z" />
+                        <path d="M0 0h24v24H0z" fill="none" />
+                        <animate attributeName="opacity" from="0.2" to="1" dur="1s" repeatCount="indefinite" />
                     </svg>
-                )}
+                ) : null}
             </div>
 
 
--- front/src/Pages/Veterinarian/My_appointments_vet/My_appointments_vet.js
+++ front/src/Pages/Veterinarian/My_appointments_vet/My_appointments_vet.js
@@ -9,14 +9,11 @@ import { Componets_My_appointments_vet } from '../../../Componets'
 const My_appointments_vet = () => {
 
   const [Info, setInfo] = useState([])
-  // const [InfoID, setInfoID] = useState([])
-
 
   useEffect(() => {
     axios.get('http://localhost:3001/clinic/show_appointment_vet', { withCredentials: true })
       .then(res => {
         setInfo(res.data.result);
-        // setInfoID(res.data.id)
       })
       .catch(err => {
         console.log(err);
@@ -31,11 +28,28 @@ const My_appointments_vet = () => {
           {Info.map((d, i) => {
             return (
               <div key={i}>
-                <Componets_My_appointments_vet Type={d.service} clinic={d.c_name}
-                  animal={d.name} date={d.datebooking} time={d.timebookig} href={`Diagnosis/${d.id_b}/${d.id}`} />
+                <Componets_My_appointments_vet
+                  Type={d.service}
+                  clinic={d.c_name}
+                  animal={d.name}
+                  date={d.datebooking}
+                  time={d.timebookig}
+                  href={`Diagnosis/${d.id_b}/${d.id}`}
+                  test={"d."}
+                />
               </div>
             )
           })}
+          {/* <Componets_My_appointments_vet Type={""} test={true} clinic={""}
+            animal={""} date={""} time={""} href={""} />
+          <Componets_My_appointments_vet Type={""} test={true} clinic={""}
+            animal={""} date={""} time={""} href={""} />
+          <Componets_My_appointments_vet Type={""} clinic={""}
+            animal={""} date={""} time={""} href={""} />
+          <Componets_My_appointments_vet Type={""} clinic={""}
+            animal={""} date={""} time={""} href={""} />
+          <Componets_My_appointments_vet Type={""} clinic={""}
+            animal={""} date={""} time={""} href={""} test={true}/> */}
 
 
 
