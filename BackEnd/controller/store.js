ec0d0c24f367ec6d9263dffaabdcde6dea73f6bc
Dragonmo <moohamad20-01@hotmail.com>
2024-03-27 23:17:06 +0300

    12345678

--- BackEnd/controller/store.js
+++ BackEnd/controller/store.js
@@ -1,6 +1,7 @@
 const models=require("../models")
 const db=require("../dbb/db")
 function new_arrivle(req,res){
+    if(req.session.username){
     sql='SELECT * FROM `proudact` WHERE DATEDIFF(CURRENT_DATE(),proudact.date_added)<10'
     db.query(sql,(error,result)=>{
         if(error){console.log(error)}
@@ -10,6 +11,24 @@ function new_arrivle(req,res){
         }
     })
 }
+else res.json({valid:false})
+}
+function sort_by_animal(req,res){
+    if(req.session.username)
+ {   animal=req.body.animal
+    db.query('SELECT * FROM proudact WHERE animal_type LIKE ?', ['%'+animal+'%'], (error, results) => {
+    if (error) {
+            console.error(error);
+        } else {
+            res.json({valid:true,results})
+        }
+
+    });}
+    else res.json({valid:false})
+}
+function 
+
 module.exports={
     new_arrivle:new_arrivle,
+    sort_by_animal
 }
--- BackEnd/routes/storeR.js
+++ BackEnd/routes/storeR.js
@@ -4,6 +4,7 @@ const store=require("../controller/store")
 const imageUploader = require('../helper/image_uploader')
 
 router.get('/new_arrivle',store.new_arrivle);
+router.get('/sort_by_animal',store.sort_by_animal);
 
 
-module.exports=router
+module.exports=router 
