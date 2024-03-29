9082eb39b538d5174143c6d1574d78b8632692a4
Dragonmo <moohamad20-01@hotmail.com>
2024-03-29 05:09:35 +0300

    j

--- BackEnd/routes/storeR.js
+++ BackEnd/routes/storeR.js
@@ -6,6 +6,7 @@ const imageUploader = require('../helper/image_uploader')
 router.get('/new_arrivle',store.new_arrivle);
 router.get('/sort_by_animal',store.sort_by_animal);
 router.get('/search_by',store.search_salary_and_category);
+router.get('/open_proudact/:id',store.open_proudact);
 
 
-module.exports=router 
+module.exports=router  
