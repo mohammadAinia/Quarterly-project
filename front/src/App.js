cf7daa7d43544bd58f7cbfb4adaa1792987e72f3
mohammadAinia <mmyyttt@gmail.com>
2024-04-05 03:57:40 +0300

    jk

--- front/src/App.js
+++ front/src/App.js
@@ -169,7 +169,9 @@ const App = () => {
             <Route path='choose_product/:id' element={<Choose_Product />} />
             <Route path='Product_Assortment/:id' element={<Product_Assortment />} />
             <Route path='Add_product_store' element={<Add_product_store />} />
-            <Route path='shop_by_pet' element={<Shop_By_Pet />} />
+            <Route path='shop_by_pet/:id' element={<Shop_By_Pet />} />
+            <Route path='shop_by_pet/Product_Assortment/:id' element={<Product_Assortment />} />
+
 
 
           </Routes>
